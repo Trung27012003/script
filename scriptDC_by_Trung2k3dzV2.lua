275f0c7143d6a3a7aa8b163967235aad7dcb1b42
Trung27012003 <Trungtvph28671@fpt.edu.vn>
2023-02-21 01:31:57 +0700

    Update scriptDC_by_Trung2k3dzV2.lua

--- scriptDC_by_Trung2k3dzV2.lua
+++ scriptDC_by_Trung2k3dzV2.lua
@@ -25,7 +25,7 @@ while (true) do
             '6.   🔁  Change Dragon 🐉 --> 🐍 🔁 ', '7.   🔘   Other',
             '❌     Exit     ❌ '
         }, Last,
-                         '                           Dragon City Tools\nVersion: Pro \nTime Login:'.. os.date("%H:%M:%S %d-%m-%Y") ..')
+                         '                           vcl')
         if menu == 1 then Start1() end
         if menu == 2 then Start2() end
         if menu == 3 then Start3() end
